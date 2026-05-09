d145f6a78881049145a1d28bfeebd577bdfa6040
NestorAndres1215 <atiroandres@gmail.com>
2026-05-09 11:22:49 -0500

    subiendo

---
+++ frontend/src/app/admin/entrenadores/listar/page.tsx
@@ -0,0 +1,11 @@
+import AdminLayout from "../../layout/AdminLayout";
+
+export default function EntrenadorLista() {
+    return(
+        <AdminLayout>
+            <div className="container">
+                holaaa
+            </div>
+        </AdminLayout>
+    )
+}
